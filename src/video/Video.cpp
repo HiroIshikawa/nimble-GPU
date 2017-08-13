5965503edce60de989ccf339de5de82b256eca16
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-08-13 02:08:33 -0700

    Add video streamer

---
+++ src/video/Video.cpp
@@ -0,0 +1,42 @@
+#include <thread>
+#include "Video.h"
+
+
+Video::Video(int src, int winW, int winH) {
+	this.stream(src);
+	this.stream.set(3, winW);
+	this.stream.set(4, winH);
+	std::tuple<cv::Mat, cv::Mat> reading;
+	reading = this.stream.read();
+	this.grabbed = reading[0];
+	this.frame = reading[1];
+	this.stopped = false;
+}
+
+
+void start() {
+	std::thread(this.update, args()).start();
+	return this;
+}
+
+
+void updat() {
+	while (true) {
+		if (this.stopped)
+			return
+		std::tuple<cv::Mat, cv::Mat> reading;
+		reading = this.stream.read();
+		this.grabbed = reading[0];
+		this.frame = reading[1];
+	}
+}
+
+
+void read() {
+	return this.frame;
+}
+
+
+void stop() {
+	this.stopped = true;
+}
---
+++ src/video/Video.h
@@ -0,0 +1,20 @@
+#ifndef VIDEO_H_
+#define VIDEO_H_
+
+
+class Video {
+  private:
+  	cv::VideoCapture stream;
+  	cv::Mat grabbed;
+  	cv::Mat frame;
+  	bool stopped;
+
+	public:
+		Video(int src, int winW, int winH);
+		void start();
+		void updat();
+		void read();
+		void stop();
+};
+
+#endif  // VIDEO_H_
